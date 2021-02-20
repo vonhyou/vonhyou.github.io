50cd9e9662fef53ad05736b7de1287dedcb6c06a
vonhyou <vonhyou@yeah.net>
2021-02-20 22:25:59 +0800

    del all

--- neoposr.rb
+++
@@ -1,9 +0,0 @@
-postName = Time.new.strftime("%Y-%m-%d") + "-" + ARGV[0].gsub(" ", "-")
-
-POSTPATH = "./_posts/"
-_post = File.new(POSTPATH + postName + ".md", "w")
-
-_post.syswrite("---\ntitle: #{ARGV[0]}\ntags:  []\n---")
-
-
-# puts POSTPATH + postName
